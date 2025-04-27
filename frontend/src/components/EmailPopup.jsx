863ccc2b1747110bec20f00b1dc1eccf155e7c0f
ethanman102 <ethankey.s@yahoo.com>
2025-04-27 00:34:09 -0600

    .

--- frontend/src/components/EmailPopup.jsx
+++ frontend/src/components/EmailPopup.jsx
@@ -2,6 +2,9 @@ import React, {useState} from "react"
 import "../styles/EmailPopup.css"
 
 const EmailPopup = () => {
+
+    const [characterCount, setCharacterCount] = useState(0);
+
     return(
         <div className="emailContainer">
             <div className="emailHeaderContainer">
@@ -14,7 +17,11 @@ const EmailPopup = () => {
         </div>
             <input className="inputBox" placeholder="Your Name" type="text"/>
             <input className="inputBox" placeholder="Your Email" type="email"/>
-            <textarea placeholder="Message" className="inputBox inputMessage" />
+            <textarea placeholder="Message" className="inputBox inputMessage" onChange={e => setCharacterCount(e.target.value.length)}/>
+            <div className="emailBoxFooter">
+            <div className="emailBoxDivit">{characterCount}/1000</div>
+            <button className="emailSendButton">Send</button>
+            </div>
         </div>
     )
 }
