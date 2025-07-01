fcb6b8775d4b91c7aa36c51977cde82cdc1dfd43
ethanman102 <ethankey.s@yahoo.com>
2025-06-30 23:12:30 -0600

    id

--- frontend/src/components/ImageDisplay.jsx
+++ frontend/src/components/ImageDisplay.jsx
@@ -12,7 +12,33 @@ const ImageDisplay = () => {
                     <button className="idbutton">X</button>
                 </div>
             </div>
-
+            <div className="imageDisplayBody">
+                <div className="imageDisplayColorBar">
+                    <div className="paintSquare" style={{background: 'red'}}> </div>
+                    <div className="paintSquare" style={{background: 'orange'}}> </div>
+                    <div className="paintSquare" style={{background: 'yellow'}}> </div>
+                    <div className="paintSquare" style={{background: 'lime'}}> </div>
+                    <div className="paintSquare" style={{background: 'green'}}> </div>
+                    <div className="paintSquare" style={{background: 'white'}}> </div>
+                    <div className="paintSquare" style={{background: 'black'}}> </div>
+                    <div className="paintSquare" style={{background: 'pink'}}> </div>
+                    <div className="paintSquare" style={{background: 'purple'}}> </div>
+                    <div className="paintSquare" style={{background: 'magenta'}}> </div>
+                    <div className="paintSquare" style={{background: 'blue'}}> </div>
+                    <div className="paintSquare" style={{background: 'cyan'}}> </div>
+                    <div className="paintSquare" style={{background: 'chartreuse'}}> </div>
+                    <div className="paintSquare" style={{background: 'teal'}}> </div>
+                    <div className="paintSquare" style={{background: 'navajowhite'}}> </div>
+                    <div className="paintSquare" style={{background: 'gold'}}> </div>
+                    <div className="paintSquare" style={{background: 'gray'}}> </div>
+                    <div className="paintSquare" style={{background: 'lightblue'}}> </div>
+                </div>
+                <img className="imageDisplayContent"/>
+            </div>
+            <div className="pageFooter">
+                <div className="pageBoxDivit pageBoxDivitLeft"> </div>
+                <div className="pageBoxDivit pageBoxDivitRight"> </div>
+            </div>
         </div>
     )
 }
