b913dd8aa9b2e0f27d0eb59a778c71939eccfb81
ethanman102 <ethankey.s@yahoo.com>
2025-07-06 00:34:04 -0600

    backend

---
+++ backend/api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=50, null=False, blank=False)
+    tagline = models.CharField(max_length=50, null=False, blank=False)
+    description = models.CharField(max_length=5000,null=False,blank=False)
+    created_on = models.DateTimeField()
+    views = models.IntegerField(default=0,blank=False, null=False)
+    repository = models.URLField(blank=True,null=True,default=None)
---
+++ backend/api/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Project
+
+class ProjectSerializer(serializers.ModelSerializer):
+
+
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+
+    def create(self, validated_data):
+        project = Project.objects.create(**validated_data)
+        project.save()
+        return project
+    
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title','My Cool Project')
+        instance.tagline = validated_data.get('tagline','A Personal Project')
+        instance.views = validated_data.get('views',0)
+        instance.description = validated_data.get('description','This is a personal Project that I created to test out my coding capabilities and skills')
+        instance.created_on = validated_data['created_on']
+        instance.repository = validated_data.get('repository',None)
+        instance.save()
+        return instance
+    
