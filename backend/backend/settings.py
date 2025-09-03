4f82310ef62b55004b7fb183424c50c656fdd80f
ethanman102 <ethankey.s@yahoo.com>
2025-09-03 00:09:16 -0600

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
